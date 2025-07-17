a240e501a23f1ce810b119507b7e7c2e203e6bfe
wac0705 <wac0705@gmail.com>
2025-07-17 19:22:26 +0800

    Update account.go

--- models/account.go
+++ models/account.go
@@ -1,10 +1,9 @@
-// models/account.go
 package models
 
 type Account struct {
 	ID       int    `json:"id" db:"id"`
 	Username string `json:"username" db:"username"`
-	Password string `json:"password" db:"password"`
-	Role     string `json:"role" db:"role"`
+	Password string `json:"password" db:"password"` // 密碼雜湊值
+	Role     string `json:"role" db:"role"`         // 角色名稱
 	IsActive bool   `json:"is_active" db:"is_active"`
 }
