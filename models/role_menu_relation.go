d05c9379c1584f7b237efbebc80f92c76358f3a7
wac0705 <wac0705@gmail.com>
2025-07-23 16:32:37 +0800

    Update role_menu_relation.go

--- models/role_menu_relation.go
+++ models/role_menu_relation.go
@@ -1,7 +1,6 @@
-// models/role_menu_relation.go
 package models
 
 type RoleMenuRelation struct {
-    RoleID int `json:"role_id" gorm:"primaryKey"`
-    MenuID int `json:"menu_id" gorm:"primaryKey"`
+    RoleID uint `json:"role_id" gorm:"primaryKey"`
+    MenuID uint `json:"menu_id" gorm:"primaryKey"`
 }
