885b28c7e5fc6480e017b4bea63f58a5987a47bb
wac0705 <wac0705@gmail.com>
2025-07-23 16:32:21 +0800

    Update menu.go

--- models/menu.go
+++ models/menu.go
@@ -1,12 +1,11 @@
-// models/menu.go
 package models
 
 type Menu struct {
-    ID       int     `json:"id" gorm:"primaryKey;autoIncrement"`
-    Name     string  `json:"name"`
-    Path     string  `json:"path"`
-    Icon     string  `json:"icon"`
-    ParentID *int    `json:"parent_id"`
-    OrderNo  int     `json:"order_no"`
-    IsActive bool    `json:"is_active"`
+    ID       uint   `json:"id" gorm:"primaryKey;autoIncrement"`
+    Name     string `json:"name"`
+    Path     string `json:"path"`
+    Icon     string `json:"icon"`
+    ParentID *uint  `json:"parent_id"`  // 支援 null
+    OrderNo  int    `json:"order_no"`
+    IsActive bool   `json:"is_active"`
 }
