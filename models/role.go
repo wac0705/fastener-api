4c77de7b18112d2426a5fed7e157de5d0a3d7903
wac0705 <wac0705@gmail.com>
2025-07-24 15:50:39 +0800

    Update role.go

--- models/role.go
+++ models/role.go
@@ -2,7 +2,7 @@ package models
 
 type Role struct {
     ID   uint   `json:"id" gorm:"primaryKey"`
-    Name string `json:"name"`
+    Name string `json:"name" gorm:"unique"` // <-- 在這裡添加 gorm:"unique"
     // 你有 permissions 欄位的話也可以加
     Permissions []string `json:"permissions" gorm:"type:jsonb"`
 }
