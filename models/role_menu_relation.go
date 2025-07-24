77116e076f6cba2775e6980f6653ed9bba47ce20
wac0705 <wac0705@gmail.com>
2025-07-24 16:01:37 +0800

    Update role_menu_relation.go

--- models/role_menu_relation.go
+++ models/role_menu_relation.go
@@ -1,6 +1,10 @@
 package models
 
+// RoleMenuRelation 代表角色與菜單的關聯
 type RoleMenuRelation struct {
-    RoleID uint `json:"role_id" gorm:"primaryKey"`
-    MenuID uint `json:"menu_id" gorm:"primaryKey"`
+    RoleID uint `gorm:"primaryKey"` // 角色 ID，作為複合主鍵的一部分
+    MenuID uint `gorm:"primaryKey"` // 菜單 ID，作為複合主鍵的一部分
+    // 您可以根據需要在此處添加其他字段，例如：
+    // IsActive  bool `gorm:"default:true"` // 表示此關聯是否啟用
+    // OrderNo   int  // 如果菜單在特定角色下有顯示順序
 }
